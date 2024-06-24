eea38b01eacdd82ecb8915d289058a285c85e170
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-06-24 11:16:33 +0530

    Create Ranjit.java

---
+++ Ranjit.java
@@ -0,0 +1,47 @@
+class Ranjit{
+public static void main(String args[])
+{
+    String groom = "Ranjit";
+    String bride = "Sanjeet"; 
+    // System.out.println(name);
+    // System.out.println("text size: " + name.length());
+    // System.out.println(name.toUpperCase());
+    // System.out.println(name.toLowerCase());
+    // System.out.println(name.indexOf("i"));
+    // System.out.println(String.join(" and ", bride, groom));
+    System.out.println(groom.replace("i", "ee"));
+
+    // int a = 21;
+
+    // System.out.println(!(a >= 18 || a<=50 ));
+// System.out.println("Ranjit");
+
+    // int a = 50;
+    // System.out.println(a);
+
+    // byte b = 127;
+    // System.out.println(b);
+
+    // short c = 32767;
+    // System.out.println(c);
+
+    // long d = 922_33720_368547_75807L;
+    // System.out.println(d);
+
+    // float e = 125746546181661471419687457845884198260.554616161f; 
+    // System.out.println(e);
+
+    // double f = 69558488996656564656546464165146697581468461874618471674918719787197198719881168416841686841684641684168341641864154510.5;
+    // System.out.println(f);
+
+    // boolean g = true;
+    // System.out.println(g);
+ 
+    // char h = 'a';
+    // System.out.println(h);
+
+    // String i = "Ranjit";
+    // System.out.println(i);
+
+}
+}  
