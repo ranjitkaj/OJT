6aa8de149a73ad8fbe3911a3d9b5ec8e5a44340d
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-06-24 11:13:26 +0530

    Create Gater.java

---
+++ Gater.java
@@ -0,0 +1,26 @@
+class kumar{
+    private int age; 
+    private String name;
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
+
+public class Gater {
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
