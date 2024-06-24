bbbbacf9650509ed3d19b0f01c9af0dd7af4f8e1
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-06-24 11:16:08 +0530

    Create exe.java

---
+++ exe.java
@@ -0,0 +1,17 @@
+public class exe {
+
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        String a = "Ranjit";
+        int b = 0;
+        int c = 10;
+        String z = "Ranjit";
+
+        // System.out.println(a/c);  // Arithmetic Exception
+        
+
+
+        System.out.println("Hello World!");
+}
+
+}
