31a39a128f2f9d54425e9c11a99e0704334b225a
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-06-24 11:16:52 +0530

    Create Review.java

---
+++ Review.java
@@ -0,0 +1,21 @@
+public class Review {
+    public static void main(String[] args) {
+        int year = 2001;
+        String title = "Shrek";
+        char genre = 'C';
+        double runtime = 1.58;
+        boolean isPG = true;
+
+        System.out.println(year);
+
+        String 1stName = "Samira";
+        String blah = "Smith";
+        String .com = "samira@google.com";
+        int salaryexpectation = 100000;
+        int year_of_birth = 1955;
+    
+        System.out.println("The program runs!");
+        }
+
+    }
+
