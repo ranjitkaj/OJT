6ed3ebbace9b4e8b8ce8719b9214911784be2ce2
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-06-24 11:13:20 +0530

    Create a.java

---
+++ class/math1/support/a.java
@@ -0,0 +1,13 @@
+package math1.support;
+
+public class a {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+}
