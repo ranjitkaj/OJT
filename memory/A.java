e3ca75220c3fcc3f7806ad50431cd706e766214b
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-06-24 11:13:35 +0530

    Create A.java

---
+++ memory/A.java
@@ -0,0 +1,17 @@
+package memory;
+class Ranjit {
+    int age=10;
+    String name="Ranjit";
+}
+
+public class A {
+
+    public static void main(String[] args) {
+
+        int age = 10;
+        String name = "Ranjit";
+        
+
+    }
+    
+}
