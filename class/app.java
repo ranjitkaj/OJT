e5c883ede939dc456a990fd89ddb0707df23fae5
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-06-24 11:13:16 +0530

    Create app.java

---
+++ class/app.java
@@ -0,0 +1,13 @@
+import math1.math;
+import math1.support.a;
+
+public class app {
+
+    public static void main(String[] args) {
+        
+        System.out.println(add(10,20));
+        System.out.println(sub(10,20));
+
+
+    }
+}
