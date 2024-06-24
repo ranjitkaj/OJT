ca5d1cc23fddd43002641b6718b5f329cea107e3
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-06-24 11:13:04 +0530

    Create math.java

---
+++ class/math1/math.java
@@ -0,0 +1,12 @@
+package math1;
+public class math {
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+    public int mult(int a, int b) {
+        return a * b;
+    }
+
+    
+}
