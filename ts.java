5c3bd8e2a493eed89816c441bea9194ea2dc337f
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-06-24 11:17:02 +0530

    Create ts.java

---
+++ ts.java
@@ -0,0 +1,9 @@
+public class ts {
+    public static void main(String[] args) {
+        double a = 10.0;
+        int b = (short) a;
+
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
